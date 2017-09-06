f6aaf9867954bb0a60543fbe6f10c375fd06bd34
yoskeoka <yoske95@gmail.com>
2017-09-06 22:09:26 +0900

    biased probability

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 (function() {
   "use strict";
+
+  // generate results
+  var resultsSet = {
+    "大吉": 15,
+    "吉": 30,
+    "末吉": 20,
+    "凶": 20,
+    "大凶": 15
+  };
+  var results = [];
+  for (var k in resultsSet) {
+    for (var i = 0; i < resultsSet[k]; i++) {
+      results.push(k);
+    }
+  }
+
   var btn = document.getElementById('btn');
+
   btn.addEventListener("click", function() {
-    var results = ["大吉", "吉", "末吉", "凶"];
     var n = Math.floor(Math.random() * results.length);
     this.textContent = results[n];
   });
